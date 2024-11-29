c5deb9545de9a47ff375909166b8ca87674ee25a
IEVGEN SHAPOSHNYK <jekas@MacBook-Air-IEVGEN.local>
2024-11-30 00:22:11 +0200

    Docker with Laravel11 + Nuxtjs 3

--- backend/run_transaction_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Starting transactions check at $(date)" >> /var/log/cron.log
-
-/usr/local/bin/php /var/www/html/artisan app:transactions-check >> /var/log/cron.log 2>&1
-
-echo "Completed transactions check at $(date)" >> /var/log/cron.log
-
